aeb8fda0bd2070e99710d2bf06e3effb9cbe255e
Cubelaster <david.cubela@gmail.com>
2018-11-04 11:45:14 +0100

    Working CORS and API

---
+++ NgSchoolsBackend/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NgSchoolsBackend.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public JsonResult GetAll()
+        {
+            return new JsonResult(new string[] { "Kita" });
+        }
+    }
+}
--- NgSchoolsBackend/Startup.cs
+++ NgSchoolsBackend/Startup.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace NgSchoolsBackend
 {
@@ -26,6 +19,18 @@ namespace NgSchoolsBackend
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddCors(options =>
+            {
+                // TODO: Add right Origin
+                options.AddPolicy("Policy",
+                    builder => builder
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials()
+                    .AllowAnyOrigin()
+                );
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +44,8 @@ namespace NgSchoolsBackend
             {
                 app.UseHsts();
             }
+
+            app.UseCors("Policy");
 
             app.UseHttpsRedirection();
             app.UseMvc();
