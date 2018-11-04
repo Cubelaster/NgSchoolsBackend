42185e550aa9a6cefa38c08aa1272eed44563f9b
Cubelaster <david.cubela@gmail.com>
2018-11-04 14:11:12 +0100

    Created an initial migration and started the code first approach

--- NgSchoolsBackend/Startup.cs
+++ NgSchoolsBackend/Startup.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NgSchoolsDataLayer.Context;
 
 namespace NgSchoolsBackend
 {
@@ -18,6 +20,9 @@ namespace NgSchoolsBackend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<NgSchoolsContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("NgSchoolsConnection"), opts => opts.MigrationsAssembly("NgSchoolsDataLayer")));
+
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddCors(options =>
--- NgSchoolsDataLayer/Context/NgSchoolsContext.cs
+++ NgSchoolsDataLayer/Context/NgSchoolsContext.cs
@@ -4,5 +4,6 @@ namespace NgSchoolsDataLayer.Context
 {
     public class NgSchoolsContext : DbContext
     {
+        public NgSchoolsContext(DbContextOptions<NgSchoolsContext> options) : base(options) { }
     }
 }
