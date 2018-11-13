00b3e4164e9bd366a16bc133691e3ff96809c93c
Cubelaster <david.cubela@gmail.com>
2018-11-13 20:42:17 +0100

    small thingy

--- NgSchoolsBusinessLayer/Models/Dto/UserDto.cs
+++ NgSchoolsBusinessLayer/Models/Dto/UserDto.cs
@@ -10,9 +10,12 @@ namespace NgSchoolsBusinessLayer.Models.Dto
     public class UserDto
     {
         public Guid Id { get; set; }
+        public string Email { get; set; }
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public List<Claim> Claims { get; set; }
+
+        public string UserRoles => Claims != null ? string.Join(", ", Claims) : "";
     }
 }
