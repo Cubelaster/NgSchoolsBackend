1298df4ff87d317d102075677c06fb0325d55431
Cubelaster <david.cubela@gmail.com>
2019-01-26 14:35:38 +0100

    fix themes

---
+++ NgSchoolsDataLayer/Migrations/20190126133506_Theme_Add_Missing_Props.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NgSchoolsDataLayer.Migrations
+{
+    public partial class Theme_Add_Missing_Props : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HoursNumber",
+                table: "Themes",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HoursNumber",
+                table: "Themes");
+        }
+    }
+}
