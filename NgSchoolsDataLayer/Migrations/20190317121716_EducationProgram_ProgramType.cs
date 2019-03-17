a965fdba269f70c1f3d2787e355239a7308ce131
Cubelaster <david.cubela@gmail.com>
2019-03-17 14:25:53 +0100

    Fix for missing program type

---
+++ NgSchoolsDataLayer/Migrations/20190317121716_EducationProgram_ProgramType.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace NgSchoolsDataLayer.Migrations
+{
+    public partial class EducationProgram_ProgramType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProgramType",
+                table: "EducationPrograms",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProgramType",
+                table: "EducationPrograms");
+        }
+    }
+}
