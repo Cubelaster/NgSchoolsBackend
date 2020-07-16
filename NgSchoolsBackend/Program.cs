c97e9049834951c0dccdb3fcb9df4792f6b0bf4a
Cubelaster <david.cubela@gmail.com>
2020-07-16 20:12:48 +0200

    Update Version

--- NgSchoolsBackend/Program.cs
+++ NgSchoolsBackend/Program.cs
@@ -19,7 +19,7 @@ namespace NgSchoolsBackend
                 DbInitializer.Initialize(services);
             }
 
-            Environment.SetEnvironmentVariable("Version", new Version("2.0.3.3").ToString());
+            Environment.SetEnvironmentVariable("Version", new Version("2.0.4.0").ToString());
 
             host.Run();
         }
